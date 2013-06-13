4e25e29d33bc81b84720dee93736a5c89e7a6472
Santiago Zabaleta <szabaleta@fullsail.edu>
2013-06-13 17:01:40 -0400

    Prompt and Alert assignment submit

---
+++ Zabaleta_Santiago_wpf/Prompt and Alert/js/script.js
@@ -0,0 +1,9 @@
+//Prompt And Alert
+
+var width = prompt("We are calculating the area of a rectangle. \nPlease Enter the width")
+var height = prompt("Please enter the height.");
+var area = width * height; //calculates with that info
+//console.log(area); //prints it out to console
+
+var result = "The area of your rectangle is " + area + " sq feet";
+alert(result);
---
+++ Zabaleta_Santiago_wpf/Values_Variables/js/script.js
@@ -0,0 +1,9 @@
+// JavaScript
+
+var yearBorn = 1955;
+/*
+console.log("Hello World!");
+console.log(yearBorn);
+    */
+
+alert(yearBorn);
