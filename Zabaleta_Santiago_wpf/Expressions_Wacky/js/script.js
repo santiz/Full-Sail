6620fa0462514eb434b9089ac790de7765e8ffe8
Santiago Zabaleta <szabaleta@fullsail.edu>
2013-06-13 23:20:27 -0400

    Expression Wacky
    Changed plan

--- Zabaleta_Santiago_wpf/Expressions_Wacky/js/script.js
+++ Zabaleta_Santiago_wpf/Expressions_Wacky/js/script.js
@@ -1,2 +1,13 @@
 //Santiago Zabaleta 6/13/13 Expression Worksheet
 
+var welcomeMessage = ("Well Hello there! Welcome to the Internet!")
+alert(welcomeMessage); //Welcoming message for user
+var name = prompt("Please enter your name below:")
+console.log("The users name is" + " " + name) //Gets the users name
+var title= ("Today, we'll pretend that you're going to build a small building. Given the fact that you'll have unlimited pieces of 2x4 pieces of wood for the foundation of the building.")
+alert(title);
+
+
+
+
+
