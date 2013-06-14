e7f602c165f1e623f4d9a4c0d59835e86e9916cf
Santiago Zabaleta <szabaleta@fullsail.edu>
2013-06-14 00:30:40 -0400

    Added Sq. Footage

--- Zabaleta_Santiago_wpf/Expressions_Wacky/js/script.js
+++ Zabaleta_Santiago_wpf/Expressions_Wacky/js/script.js
@@ -4,10 +4,15 @@ var welcomeMessage = ("Well Hello there! Welcome to the Internet!")
 alert(welcomeMessage); //Welcoming message for user
 var name = prompt("Please enter your name below:")
 console.log("The users name is" + " " + name) //Gets the users name
-var title= ("Today, we'll pretend that you're going to build a small building. Given the fact that you'll have unlimited pieces of 2x4 pieces of wood for the foundation of the building.")
+var title= ("Today, we'll pretend that you're going to build a small building. Given the fact that you'll have unlimited pieces of 4ft long pieces of wood for the foundation of the building.")
 alert(title);
-
-
-
-
-
+var length = prompt("Please enter the length, in feet, of the desired building below:");
+console.log(name + " " + "Has entered a length of" + " " + length + "ft.");
+var width = prompt("Pleas enter the width of the desired building below:");
+console.log(name + " " + "Has entered a width of" + " " + width + "ft.");
+var sqft = width * length;
+alert("Your buildings total square footage is" + " " + sqft + "ft. squared")
+var total = sqft * 4;
+console.log("The total square footage of" + " " + name + "'s " + "building is" + " " + total+ "ft. squared");
+alert(total + " " + " pieces is of wood, is the amount you'll be able to fit into the" + " " + sqft + "ft. squared building.");
+console.log(name + " " + "will need" + " " + total + " " + "pieces of wood for their building");
