a97beca843e0682327a6c8f7527b44d2fed1b056
Santiago Zabaleta <szabaleta@fullsail.edu>
2013-06-15 22:37:21 -0400

    Industry Expressions Updated

--- Zabaleta_Santiago_wpf/Expressions_Industry/js/script.js
+++ Zabaleta_Santiago_wpf/Expressions_Industry/js/script.js
@@ -1,1 +1,8 @@
-//Santiago Zabaleta 6/13/13 Expression Worksheet
+//Santiago Zabaleta 6/15/13 Expression Worksheet
+
+var welcome = ("Welcome to the Samsung Experience Store, my name is Santiago, nice to meet you...\n (You'll be asked to be inroduce yourself in the next prompt.")
+alert(welcome); // Welcoming message for the user, as if he was in the SES.
+var name = prompt("Enter your name here:");
+console.log("Santiago has welcomed in" + " " + name + " " + "into the Samsung Experience Store.");
+
+
