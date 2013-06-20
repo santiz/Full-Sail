cc45c19881316912d5c9d7710ee597034f2d1658
Santiago Zabaleta <szabaleta@fullsail.edu>
2013-06-19 21:28:07 -0400

    Ternary Operators
    Not using Ternary operator on book

--- Zabaleta_Santiago_wpf/Conditionals - With Relational Expression - Ternary Operators/js/script.js
+++ Zabaleta_Santiago_wpf/Conditionals - With Relational Expression - Ternary Operators/js/script.js
@@ -1,12 +1,12 @@
 //Conditionals Logic - Ternary Operators
 
-var gra = 48;
+var age = 11;
+var book;
 
-//if the gpa is over the min 2.0 score, the student can graduate
-/*if( gpa > 2.0){
-    console.log("You can graduate!");
+//if the child is under 10, they get Green Eggs and Ham, otherwise they get The Time Machine
+if(age <10){
+    book = "Green Eggs and Ham";
 }else{
-    console.log("GPA is too low!");
-} */
-
-(gpa > 2.0) ? console.log("You can graduate") : console.log("GPA too Low!");
+    book = "The Time Machine";
+}
+console.log(book);
