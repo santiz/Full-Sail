c3c0b6711a1a8e05739436018fa7f4001b930041
Santiago Zabaleta <szabaleta@fullsail.edu>
2013-06-19 20:26:35 -0400

    Conditonal with Expression

---
+++ Zabaleta_Santiago_wpf/Conditionals - With Expression/js/script.js
@@ -0,0 +1,13 @@
+//Conditionals Logic - With an Expression
+
+var kidHeight = 47;
+var minHeight = 48;
+var sneakerLift = 2;
+
+//if the child is old enough, print to the console "you can ride!"
+//if the kid is over 48 inches in height
+
+if(kidHeight + sneakerLift > minHeight){
+    //code performed if condition is true
+    console.log("You can ride the Coaster!")
+}
---
+++ Zabaleta_Santiago_wpf/Conditionals - With Relational Expression/js/script.js
@@ -0,0 +1,12 @@
+//Conditionals Logic - Relational Expressions
+
+var kidHeight = 40;
+var minHeight = 48;
+
+//if the child is old enough, print to the console "you can ride!"
+//if the kid is over 48 inches in height
+
+if(kidHeight > minHeight){
+    //code performed if condition is true
+    console.log("You can ride the Coaster!")
+}
