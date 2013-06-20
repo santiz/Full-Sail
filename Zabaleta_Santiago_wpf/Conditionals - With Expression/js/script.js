e60f2b8b629b93f024dfdc00c3e915e089652ab0
Santiago Zabaleta <szabaleta@fullsail.edu>
2013-06-19 20:31:55 -0400

    Changes to Conditional with Expression

--- Zabaleta_Santiago_wpf/Conditionals - With Expression/js/script.js
+++ Zabaleta_Santiago_wpf/Conditionals - With Expression/js/script.js
@@ -1,13 +1,16 @@
 //Conditionals Logic - With an Expression
 
-var kidHeight = 47;
+var kidHeight = 52;
 var minHeight = 48;
-var sneakerLift = 2;
+
 
 //if the child is old enough, print to the console "you can ride!"
 //if the kid is over 48 inches in height
 
-if(kidHeight + sneakerLift > minHeight){
+if(kidHeight > minHeight){
     //code performed if condition is true
-    console.log("You can ride the Coaster!")
+    console.log("You can ride the Coaster!");
+}
+if(kidHeight <= minHeight){
+    console.log("Sorry kid, you've got some growing to do first!");
 }
