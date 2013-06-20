6bb23bac3ee76535defec00995d9beb0f7b16fcc
Santiago Zabaleta <szabaleta@fullsail.edu>
2013-06-19 21:14:34 -0400

    Conditionals Logic - Logical Operators
    Adding lottery variable

--- Zabaleta_Santiago_wpf/Conditionals - With Relational Expression - Logical Operators/js/script.js
+++ Zabaleta_Santiago_wpf/Conditionals - With Relational Expression - Logical Operators/js/script.js
@@ -2,10 +2,10 @@
 
 var budget = 300;
 var iPhonePrice = 199.99;
-var paycheck = 200;
+var wonLottery = true;
 
 //if the price of the phone is less our budget AND if our paycheck is over 300
-if(iPhonePrice < budget && paycheck > 300){
+if(iPhonePrice < budget || wonLottery ==true){
     console.log("We can buy the phone!");
 }else{
     console.log("No phone for you!!");
