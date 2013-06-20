620d5f4d66f55ba8babc53d573fbb73d63688906
Santiago Zabaleta <szabaleta@fullsail.edu>
2013-06-19 20:18:08 -0400

    Conditionals

---
+++ Zabaleta_Santiago_wpf/Conditionals/js/script.js
@@ -0,0 +1,10 @@
+//Conditionals Logic
+
+var oldEnough = false;
+
+    //if the child is old enough, print to the      console "you can ride!"
+if(oldEnough){
+    //code performed if condition is true
+    console.log("You can ride the Coaster!")
+}
+console.log("What comes after");
