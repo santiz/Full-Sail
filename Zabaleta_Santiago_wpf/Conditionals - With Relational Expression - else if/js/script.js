74c245d551910fd6c7ba4c1dfcdfc3358d74474d
Santiago Zabaleta <szabaleta@fullsail.edu>
2013-06-19 20:41:22 -0400

    Conditionals Logic - else if
    Added parent into the mix of the code, with height

---
+++ Zabaleta_Santiago_wpf/Conditionals - With Relational Expression - else if/js/script.js
@@ -0,0 +1,19 @@
+//Conditionals Logic - else if
+
+var kidHeight = 52;
+var minHeight = 48;
+var wParentHeight = 45; //the height of the kid with the parent
+
+//if the child is old enough, print to the console "you can ride!"
+//if the kid is over 48 inches in height
+
+if(kidHeight > minHeight){
+    //you can ride
+    console.log("You can ride the Coaster!");
+}else if(kidHeight > wParentHeight ){
+    //you can ride with a prent present
+    console.log("You can ride, but only with a parent present.");
+}else{
+    //sorry you have growing to do
+    console.log("Sorry kid, you;ve got some growing to do first!");
+}
