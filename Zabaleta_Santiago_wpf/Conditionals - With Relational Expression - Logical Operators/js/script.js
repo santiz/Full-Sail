9811807fae37350633b7ff400da7113f89ddbb57
Santiago Zabaleta <szabaleta@fullsail.edu>
2013-06-19 21:11:38 -0400

    Logical Operators with Conditionals

---
+++ Zabaleta_Santiago_wpf/Conditionals - With Relational Expression - Logical Operators/js/script.js
@@ -0,0 +1,12 @@
+//Conditionals Logic - Logical Operators
+
+var budget = 300;
+var iPhonePrice = 199.99;
+var paycheck = 200;
+
+//if the price of the phone is less our budget AND if our paycheck is over 300
+if(iPhonePrice < budget && paycheck > 300){
+    console.log("We can buy the phone!");
+}else{
+    console.log("No phone for you!!");
+}
