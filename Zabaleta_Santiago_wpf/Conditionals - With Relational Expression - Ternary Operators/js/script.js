af42da19da18607ff996d0d7db55e4749f53de6b
Santiago Zabaleta <szabaleta@fullsail.edu>
2013-06-19 21:30:36 -0400

    Ternary Operators
    Using Ternary to determine book given to specifc age

--- Zabaleta_Santiago_wpf/Conditionals - With Relational Expression - Ternary Operators/js/script.js
+++ Zabaleta_Santiago_wpf/Conditionals - With Relational Expression - Ternary Operators/js/script.js
@@ -1,12 +1,8 @@
 //Conditionals Logic - Ternary Operators
 
-var age = 11;
+var age = 6;
 var book;
 
 //if the child is under 10, they get Green Eggs and Ham, otherwise they get The Time Machine
-if(age <10){
-    book = "Green Eggs and Ham";
-}else{
-    book = "The Time Machine";
-}
+book = (age <10) ? "Green Eggs and Ham" : "The Time Machine";
 console.log(book);
