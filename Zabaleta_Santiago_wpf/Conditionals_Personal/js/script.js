24dc5ee26085d1fcdfc04b8d44fab27a460c3d5c
Santiago Zabaleta <szabaleta@fullsail.edu>
2013-06-20 23:15:19 -0400

    Conditional Personal
    Added Console Log / Comments

--- Zabaleta_Santiago_wpf/Conditionals_Personal/js/script.js
+++ Zabaleta_Santiago_wpf/Conditionals_Personal/js/script.js
@@ -6,7 +6,10 @@ var gymNo = "No"; //If user does not agree to go
 var ask = prompt("Hmm, you just woke up, will you go to the gym today?\nYes or\nNo"); //The prompt asking what the user will do
 
 if(ask == gymYes){
-    alert("Well get up and go pump some iron!"); //If he agrees to go to the gym
+    alert("Well get up and go pump some iron!")
+    console.log("User stated that he does want to go to the gym"); //If he agrees to go to the gym
 }else if(ask == gymNo){
-    alert("Well keep sleeping you laz bum!"); //If the user does not want to go, gets threatening message :)
+    alert("Well keep sleeping you laz bum!")
+    console.log("User has stated that he does not want to go to the gym"); //If the user does not want to go, gets threatening message :)
 }
+
