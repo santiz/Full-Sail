b6dd1dd791f980ea21b75ce0036a0121e7a97e10
Santiago Zabaleta <szabaleta@fullsail.edu>
2013-06-24 21:10:20 -0400

    Anonymous Functions

---
+++ Zabaleta_Santiago_wpf/Functions/Functions - Anonymous Functions/js/script.js
@@ -0,0 +1,12 @@
+//Functions - Anonymous Functions
+
+
+var calcArea = function calcArea(width, height){ //defining
+    //code the function runs
+    var area = width * height;
+    return area;
+}
+
+var a = calcArea(20,30);//invoking
+
+console.log(a);
