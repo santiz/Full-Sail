2419c0fa26339439b5f9ae7a7f7785d150b3af8f
Santiago Zabaleta <szabaleta@fullsail.edu>
2013-06-24 20:17:18 -0400

    For Loops

---
+++ Zabaleta_Santiago_wpf/Loops/js/script.js
@@ -0,0 +1,26 @@
+console.log('------ Loops ------');
+
+var b = 50; //setups the index
+
+while (b >0){ //checks the condition
+    console.log(b + ' kegs on the wall');
+    b--; //increments  or decrements the index
+}
+
+console.log('------ Do While Loops ------');
+
+
+var c = 10;
+
+do{
+    console.log(c + ' kegs on the wall');
+    c--;
+}
+while(c >0);
+
+console.log('------ For Loops ------');
+
+
+for (var i = 10; i > 0; i--){
+    console.log(i + ' kegs on the wall')
+}
