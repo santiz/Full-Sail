097ddf5cde51f9e7b889fcf8bf425a0cbf4681f5
Santiago Zabaleta <szabaleta@fullsail.edu>
2013-06-24 21:03:07 -0400

    Functions vs Procedures

--- Zabaleta_Santiago_wpf/Functions/Functions - Returning Values/js/script.js
+++ Zabaleta_Santiago_wpf/Functions/Functions - Returning Values/js/script.js
@@ -1,9 +1,23 @@
 //Functions - Returning Values
 
-var total = calcArea(30,20);
+ /* var total = calcArea(30,20);
 
 function calcArea(w, h){
     var area = w * h;
     return area; //function spitting the info out
 }
-console.log(total);
+console.log(total);   */
+
+//Functions - Function vs Procedure
+
+//this is a function
+function calcArea(width, height){
+    var area = width * height;
+    return area;
+}
+
+//this is a procedure
+function calcArea(width, height){
+    var area = width * height;
+    console.log(Area); //no return
+}
