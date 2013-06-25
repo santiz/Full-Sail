a64d717a76bd0a8ef84b5fc199eb34dc3363b4e1
Santiago Zabaleta <szabaleta@fullsail.edu>
2013-06-24 20:32:42 -0400

    BAsic Functions

---
+++ Zabaleta_Santiago_wpf/Functions/js/script.js
@@ -0,0 +1,12 @@
+//Functions - Basic Structure
+
+function outptMsg(){
+    console.log("Hello World");
+}
+
+function calcArea(){
+    var width = 20;
+    var height = 30;
+    var area = width * height;
+    console.log(area);
+}
