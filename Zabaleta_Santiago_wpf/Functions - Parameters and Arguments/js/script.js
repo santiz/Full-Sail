58de2f8339b055138e401227906babc2ec4f45d9
Santiago Zabaleta <szabaleta@fullsail.edu>
2013-06-24 20:47:04 -0400

    Parameters and Arguments
    Storing it in parentheses

--- Zabaleta_Santiago_wpf/Functions - Parameters and Arguments/js/script.js
+++ Zabaleta_Santiago_wpf/Functions - Parameters and Arguments/js/script.js
@@ -1,6 +1,7 @@
 //Functions - Parameters and Arguments
+calcArea(30,20);
 
-
-function calcArea(){
-    var area
+function calcArea(w, h){ //w=30, h=20
+    var area = w * h;
+    console.log(area);
 }
