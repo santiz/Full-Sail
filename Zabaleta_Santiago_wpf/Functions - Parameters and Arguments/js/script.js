9dce9621ff49ab09f97d7d29460189a48323d581
Santiago Zabaleta <szabaleta@fullsail.edu>
2013-06-24 20:53:39 -0400

    Parameters and Arguments
    dog years using Parameters and Arguments

--- Zabaleta_Santiago_wpf/Functions - Parameters and Arguments/js/script.js
+++ Zabaleta_Santiago_wpf/Functions - Parameters and Arguments/js/script.js
@@ -1,7 +1,10 @@
 //Functions - Parameters and Arguments
-calcArea(30,20);
 
-function calcArea(w, h){ //w=30, h=20
-    var area = w * h;
-    console.log(area);
+function dogYears(age){ //paremeters
+    var dogYears = age * 7;
+    console.log("Sparky is " + dogYears + " years old.")
 }
+
+var age1 = 4;
+dogYears(age1); //arguments
+dogYears(5);
