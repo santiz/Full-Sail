3299747f70aa0add3d0780facb355b9497b49e6f
Santiago Zabaleta <szabaleta@fullsail.edu>
2013-06-27 20:01:10 -0400

    Functions Personal

--- Zabaleta_Santiago_wpf/Functions_Industry /js/script.js
+++ Zabaleta_Santiago_wpf/Functions_Industry /js/script.js
@@ -1,2 +1,4 @@
 //Santiago Zabaleta 6/26/13 Functions Industry
 
+
+
--- Zabaleta_Santiago_wpf/Functions_Personal/js/script.js
+++ Zabaleta_Santiago_wpf/Functions_Personal/js/script.js
@@ -1,1 +1,12 @@
 //Santiago Zabaleta 6/26/13 Functions Personal
+
+console.log("------ Functions Personal ------");
+
+console.log("At work, you're trying to see how many people fit in a line, when they're waiting to be attended. Every foot, is equal to a person standing. The square footage of the building is"); //Explains to the user what the setting is
+
+function calcSqft(h,l){
+    var sqft = h * l; //calculates square footage
+    console.log("According to the square footage of where people make the line, you would be able to fit " + sqft + " people on the line."); //Returns a message for the user according to what is inputted into the calcSqft below.
+}
+calcSqft(20, 1); //the lenght/height of the line, to calculate the amount of people on the line. *Assuming the 1foot per person
+
